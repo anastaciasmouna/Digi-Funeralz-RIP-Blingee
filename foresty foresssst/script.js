b71e2b4a45ffd29211080e196300fe6f9135a269
Kari-lixooo <kari.lixo0@gmail.com>
2026-06-25 18:26:51 +0100

    update

--- foresty foresssst/script.js
+++ foresty foresssst/script.js
@@ -1,3 +1,6 @@
-window.alert(" There is like, ten million things you could buy out there, but, with no cashback, Buy this OK");
+window.alert(
+  " There is like, ten million things you could buy out there, but, with no cashback, Buy this OK",
+);
+window.alert("🤑");
 //  PictureInPictureWindow(
 //   "https://i.pinimg.com/736x/10/4a/a0/104aa0819af67e8be21b976d0fcf3838.jpg" );
